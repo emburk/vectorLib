22e6c2e4cd52b1c47ad6c1b7ed6da8cc3383a0b8
Mehmet Burak Ekinci <elessar208@gmail.com>
2024-01-10 01:16:20 +0300

    initial commit

---
+++ remoteSensing.js
@@ -0,0 +1,131 @@
+const v = require("./vector")
+
+
+function getGeodeticNormal(ecef){  
+  const R_EQ = 6378.137; // equatorial radius km
+  const R_POLAR = R_EQ * (1 - 1 / 298.257223563); // polar radius km R_EQ*(1-flattening)
+  let n = v.vec(
+    (R_POLAR / R_EQ) * ecef.x,
+    (R_POLAR / R_EQ) * ecef.y,
+    (R_EQ / R_POLAR) * ecef.z
+  );
+  n = v.getUnit(n);
+  return n;
+};
+
+
+module.exports = {
+
+getEarthIntersect: function(uce, re) {
+  const a = 6378.137; // equatorial radius km
+  const b = a * (1 - 1 / 298.257223563); // polar radius km R_EQ*(1-flattening)
+
+  const d2 = uce.x * uce.x + uce.y * uce.y + ((a * a) / b / b) * uce.z * uce.z;
+  const d1 =
+    2 * (uce.x * re.x + uce.y * re.y + ((a * a) / b / b) * uce.z * re.z);
+  const d0 =
+    re.x * re.x + re.y * re.y + ((a * a) / b / b) * re.z * re.z - a * a;
+  const disc = d1 * d1 - 4 * d0 * d2;
+  // console.log(uce,re,d2,d1,d0,disc)
+  if (disc <= 0) {
+    console.log("vector is not pointing to the ellipsoid body!");
+    return null;
+  } else {
+    const k = (-d1 - Math.sqrt(disc)) / 2 / d2;
+    if (k < 0) {
+      console.log("negative vector is pointing to the ellipsoid body!");
+      return null;
+    }
+    return v.add(re, v.mult(k, uce));
+  }
+},
+
+getOrbit2Ecef: function(re, ve) {
+  // note: tirs2itrf conversion may be used for more accurate orbit 2 ecef conversation
+  const weie = v.vec(
+    0,
+    0,
+    (2 * Math.PI) / (23 * 60 * 60 + 56 * 60 + 4.098903691)
+  );
+  const D_I_rSatEcef = v.add(ve, v.cross(weie, re));
+  const u3o_e = v.mult(-1, v.getUnit(re));
+  const u2o_e = v.getUnit(v.cross(u3o_e, D_I_rSatEcef));
+  const u1o_e = v.getUnit(v.cross(u2o_e, u3o_e));
+  return v.Mbyv(u1o_e, u2o_e, u3o_e);
+},
+
+// return lat lng object in degrees
+// algorithm is exact when input is on the ellipsoid
+// if satellite ecef is used iterational model should be implemented
+ecef2ll: function(ecef) {
+  const n = getGeodeticNormal(ecef);
+  return {
+    lat: (Math.asin(n.z) / Math.PI) * 180,
+    lng: (Math.atan2(n.y, n.x) / Math.PI) * 180,
+  };
+},
+
+getGeodeticNormal,
+
+// haversine formula for distance in great circle
+getHaversineDistance: function(lat1,lon1,lat2,lon2){
+    const slat = Math.sin((lat2 - lat1)/2);
+    const slon = Math.sin((lon2 - lon1)/2);
+    return 2*6378.137*Math.asin( Math.sqrt(
+        slat* slat + Math.cos(lat1)*Math.cos(lat2)*slon*slon));
+},
+
+
+
+// ecef2lla  - convert earth-centered earth-fixed (ECEF)
+//             in cartesian coordinates (km)
+//             to geodetic lat (deg), lon (deg) and altitude (m)
+//
+// Notes: (1) This function assumes the WGS84 model.
+//        (2) Latitude is customary geodetic (not geocentric).
+//        (3) Inputs may be scalars, vectors, or matrices of the same
+//            size and shape. Outputs will have that same size and shape.
+//        (4) Tested but no warranty; use at your own risk.
+//        (5) Michael Kleder, April 2006
+ecef2lla: function(rEcef){
+
+  // conver to meters
+const x = rEcef.x*1000;
+const y = rEcef.y*1000;
+const z = rEcef.z*1000;
+
+// WGS84 ellipsoid constants:
+const a = 6378137;
+const e = 8.1819190842622e-2;
+
+// calculations:
+const b   = Math.sqrt(a*a*(1-e*e));
+const ep  = Math.sqrt((a*a-b*b)/(b*b));
+const p   = Math.sqrt(x*x+y*y);
+const th  = Math.atan2(a*z,b*p);
+let lon = Math.atan2(y,x);
+let lat = Math.atan2((z+ep*ep*b*Math.sin(th)**3),(p-e*e*a*Math.cos(th)**3));
+const N   = a/Math.sqrt(1-e*e*Math.sin(lat)**2);
+let alt = p/Math.cos(lat)-N;
+
+// return lon in range [0,2*pi)
+lon = (lon)%(2*Math.PI);
+
+// correct for numerical instability in altitude near exact poles:
+// (after this correction, error is about 2 millimeters, which is about
+// the same as the numerical precision of the overall function)
+
+if (Math.abs(x)<1 & Math.abs(y)<1)
+  alt = (Math.abs(z)-b);
+
+
+// return lat lon alt
+return {
+  lat: lat*180/Math.PI, // in degrees
+  lng: lon*180/Math.PI, // in degrees
+  alt: alt // in meters
+}
+}
+
+
+}
---
+++ vector.js
@@ -0,0 +1,202 @@
+// Math library, TODO move to another file:
+function vec(x, y, z) {
+  return { x: x, y: y, z: z };
+}
+function getAngleABabAC(a, b, AC) {
+  const bb = -2 * a * Math.cos(AC);
+  const sdelta = Math.sqrt(bb * bb - 4 * (a * a - b * b));
+  const c = (-bb - sdelta) / 2;
+  return Math.acos((a * a + b * b - c * c) / a / b / 2);
+}
+
+function getAngleACabAB(a, b, ab) {
+  const c2 = a * a + b * b - 2 * a * b * Math.cos(ab);
+  const c = Math.sqrt(c2);
+  const ac = Math.acos((a * a + c2 - b * b) / (2 * a * c));
+  return ac;
+}
+
+function getNorm(v) {
+  return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+function getUnit(v) {
+  const n = getNorm(v);
+  return {
+    x: v.x / n,
+    y: v.y / n,
+    z: v.z / n,
+  };
+}
+function cross(v1, v2) {
+  return {
+    x: v1.y * v2.z - v1.z * v2.y,
+    y: v1.z * v2.x - v1.x * v2.z,
+    z: v1.x * v2.y - v1.y * v2.x,
+  };
+}
+function dot(v1, v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+function substract(v1, v2) {
+  return { x: v1.x - v2.x, y: v1.y - v2.y, z: v1.z - v2.z };
+}
+
+function add(v1, v2) {
+  return { x: v1.x + v2.x, y: v1.y + v2.y, z: v1.z + v2.z };
+}
+
+function mult(k, v) {
+  return { x: k * v.x, y: k * v.y, z: k * v.z };
+}
+
+function getOnPlane(v, v1, v2) {
+  // return on plane component of vector v to the plane def by v1 and v2
+  const u = getUnit(cross(v1, v2));
+  return substract(v, mult(dot(v, u), u));
+}
+
+function getNormalToVec(v, v1) {
+  // return component of v perpendicular to v1
+  const u = getUnit(v1);
+  return substract(v, mult(dot(v, u), u));
+}
+
+function rotate(v, u, th) {
+  // rotate v vector in u unit vector axis of rotation in th radians
+  const c = Math.cos(th);
+  const s = Math.sin(th);
+  const cc = 1 - c;
+  return {
+    x:
+      (c + u.x * u.x * cc) * v.x +
+      (u.x * u.y * cc - u.z * s) * v.y +
+      (u.x * u.z * cc + u.y * s) * v.z,
+    y:
+      (u.y * u.x * cc + u.z * s) * v.x +
+      (c + u.y * u.y * cc) * v.y +
+      (u.y * u.z * cc - u.x * s) * v.z,
+    z:
+      (u.z * u.x * cc - u.y * s) * v.x +
+      (u.z * u.y * cc + u.x * s) * v.y +
+      (c + u.z * u.z * cc) * v.z,
+  };
+}
+
+// quaternion and kinematics:
+function quat(qx, qy, qz, qs) {
+  return {
+    x: qx,
+    y: qy,
+    z: qz,
+    s: qs,
+  };
+}
+
+function qtn2A(q) {
+  return [
+    [
+      q.x * q.x - q.y * q.y - q.z * q.z + q.s * q.s,
+      2 * (q.x * q.y + q.z * q.s),
+      2 * (q.x * q.z - q.y * q.s),
+    ],
+    [
+      2 * (q.x * q.y - q.z * q.s),
+      -q.x * q.x + q.y * q.y - q.z * q.z + q.s * q.s,
+      2 * (q.y * q.z + q.x * q.s),
+    ],
+    [
+      2 * (q.x * q.z + q.y * q.s),
+      2 * (q.y * q.z - q.x * q.s),
+      -q.x * q.x - q.y * q.y + q.z * q.z + q.s * q.s,
+    ],
+  ];
+}
+
+function MxM(M1, M2) {
+  return [
+    [
+      M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0] + M1[0][2] * M2[2][0],
+      M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1] + M1[0][2] * M2[2][1],
+      M1[0][0] * M2[0][2] + M1[0][1] * M2[1][2] + M1[0][2] * M2[2][2],
+    ],
+    [
+      M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0] + M1[1][2] * M2[2][0],
+      M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1] + M1[1][2] * M2[2][1],
+      M1[1][0] * M2[0][2] + M1[1][1] * M2[1][2] + M1[1][2] * M2[2][2],
+    ],
+    [
+      M1[2][0] * M2[0][0] + M1[2][1] * M2[1][0] + M1[2][2] * M2[2][0],
+      M1[2][0] * M2[0][1] + M1[2][1] * M2[1][1] + M1[2][2] * M2[2][1],
+      M1[2][0] * M2[0][2] + M1[2][1] * M2[1][2] + M1[2][2] * M2[2][2],
+    ]
+  ];
+}
+
+// matrix vector multiplication
+function Mxv(M, v) {
+  return {
+    x: M[0][0] * v.x + M[0][1] * v.y + M[0][2] * v.z,
+    y: M[1][0] * v.x + M[1][1] * v.y + M[1][2] * v.z,
+    z: M[2][0] * v.x + M[2][1] * v.y + M[2][2] * v.z,
+  };
+}
+
+// matrix scalar multiplication
+function Mxs(M, s) {
+  return [
+    [M[0][0] * s, M[0][1] * s, M[0][2] * s],
+    [M[1][0] * s, M[1][1] * s, M[1][2] * s],
+    [M[2][0] * s, M[2][1] * s, M[2][2] * s]
+  ];
+}
+
+// matrix transpose
+function MT(M) {
+  return [
+    [M[0][0], M[1][0], M[2][0]],
+    [M[0][1], M[1][1], M[2][1]],
+    [M[0][2], M[1][2], M[2][2]],
+  ];
+}
+
+// construct matrix by unit vectors:
+function Mbyv(u1, u2, u3) {
+  return [
+    [u1.x, u2.x, u3.x],
+    [u1.y, u2.y, u3.y],
+    [u1.z, u2.z, u3.z],
+  ];
+}
+
+function eye() {
+  return [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ];
+}
+
+module.exports = {
+  vec,
+  getAngleABabAC,
+  getAngleACabAB,
+  getNorm,
+  getUnit,
+  cross,
+  dot,
+  substract,
+  add,
+  mult,
+  getOnPlane,
+  getNormalToVec,
+  rotate,
+  quat,
+  qtn2A,
+  MxM,
+  Mxv,
+  Mxs,
+  MT,
+  Mbyv,
+  eye
+}
